9cf18ebb08c149d4db1ec624c76a56ad85a7bdcc
algorithmsunlock <126156874+algorithmsunlock@users.noreply.github.com>
2023-03-08 16:05:03 +0530

    Add files via upload

---
+++ Spepage.js
@@ -0,0 +1,12 @@
+function result(){
+        var score=0;
+        if (document.getElementById("correct1").checked) {
+          score++;
+       location.href="/specialc.html";
+        } 
+        if (document.getElementById("inc1").checked) {
+          score++;
+          document.write("Acess denied");
+        }
+        if document.getElementById("inc2")
+      }
